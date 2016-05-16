b3958c9199463df42fb251a9db3d26fe1c6b93f6
Carlos Mercado <civan.cim@gmail.com>
2016-05-15 21:25:56 -0500

    added port

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ let app = require('express')(),
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+app.set('port', (process.env.PORT || 5000));
+
 // use res.render to load up an ejs view file
 
 // index page 
@@ -13,8 +15,6 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
-let port = process.env.PORT | 3000
-
-app.listen(port, () => {
-    console.log('started :3000');
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
